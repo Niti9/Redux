8b6d93f39625dab2ec9b52e455e54938a3f20891
Niti9 <nnegi8298@gmail.com>
2023-06-13 16:23:51 +0530

    thunk middleware maan lo isse

--- index.js
+++ index.js
@@ -4,22 +4,27 @@
  * then install redux = npm install Redux
  * Now we install a middleware = npm install redux-logger 
  * to use applyMiddleware in Redux
+ * then install = npm i -g json-server
+ * to run json server
+ * then run this command in cmd because terminal will not run this = json-server db.json
 */
 
 
 
 /** to increment amoumt */
 
-//Now create function for action called action creator
+//create thug
 
-import {createStore, applyMiddleware} from 'redux'  // import aplyMiddleware
+import { createStore, applyMiddleware } from 'redux'  // import aplyMiddleware
 import logger from 'redux-logger' //import logger 
+import axios from 'axios';
 
 // action name constants (used to resolve the conflict kyunki increment , decrement wagarh bhot baar repeat ho rha hai 
 // if condition  mein aur action creator aur dispatch mein bhi)
 const inc = 'increment'; // string(increment) convert into constant
 const dec = 'decrement';
-const incByAmo= 'incrementByAmount';
+const incByAmo = 'incrementByAmount';
+const init = 'init';
 
 
 //to make Store
@@ -28,35 +33,40 @@ const store = createStore(reducer, applyMiddleware(logger.default)); // using lo
 
 
 //initialize state here
-function reducer(state={amount: 1}, action){ // state means previous state and action means the action we doing like add, remove etc.
+function reducer(state = { amount: 1 }, action) { // state means previous state and action means the action we doing like add, remove etc.
 
     //immutable (means the original state not change but copy of original create and change)
-    if(action.type === inc)
-    {
-        return {amount: state.amount+1} 
-    }
-    if(action.type=== dec)
-    {
-        return {amount: state.amount -1}
-    }
-    if(action.type=== incByAmo ){
+    switch (action.type) {
+        case init:
+            return { amount: action.payload };
 
-        return {amount: state.amount + action.payload}
+        case inc:
+            return { amount: state.amount + 1 };
+
+        case dec:
+            return { amount: state.amount - 1 };
+
+        case incByAmo:
+            return { amount: state.amount + action.payload };
+
+        default:
+            return state
     }
-    
-    return state 
 }
 
 
 //Action Creator
-function increment(){
-    return{type: inc}
+function initUser(value) {
+    return { type: init, payload: value}
 }
-function decrement(){
-    return{type: dec}
+function increment() {
+    return { type: inc }
 }
-function incrementByAmount(value){
-    return{type: incByAmo , payload:value}
+function decrement() {
+    return { type: dec }
+}
+function incrementByAmount(value) {
+    return { type: incByAmo, payload: value }
 }
 
 
@@ -70,18 +80,9 @@ console.log(store.getState())
 store.dispatch(increment()) // increment to increase value
 store.dispatch(decrement()) // decrement  to decrease the value
 store.dispatch(incrementByAmount(4)) // here payload must be given it will like a gap between the values
+store.dispatch(initUser(400)) // here payload must be given it will make gap b/w values
 
 
 // new state(c hanges)
 console.log(store.getState())
 
-
-/* output:
-{ amount: 1 }
- action increment @ 14:48:10.794
-   prev state { amount: 1 }
-   action     { type: 'increment' }
-   next state { amount: 2 }
-{ amount: 2 }
-
-*/
