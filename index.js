f4758973bf39b61a4f0ef3f786ea7e2a48a15473
Niti9 <nnegi8298@gmail.com>
2023-06-13 14:49:22 +0530

    using middleware

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
-/** create indez.js file
+/** create index.js file
  * then create package.json through type command in
  * terminal = npm init -y
  * then install redux = npm install Redux
- */
-
-
-/**  Starting of Redux(pure redux)
- * redux is used to make  a state(koi data store karne ke liye)
- * and then we can do any action on that state
- * hum usse change kar sakte hai use kar sakte hai kahin bhi
-*/
-
-/* example of normal redux code
-
-// import createStore
-import {createStore} from 'redux'  // here createStore already depict or remove but we will use it
-
-//to make Store
-const store = createStore(reducer); // reducer are some functions
-
-function reducer(state, action){ // state means previous state and action means the action we doing like add, remove etc.
-
-    return state // to get new state
-}
-
-//global state
-console.log(store.getState())
-
-output:
-undefined
-
+ * Now we install a middleware = npm install redux-logger 
+ * to use applyMiddleware in Redux
 */
 
 
 
-/** to increment amout */
+/** to increment amoumt */
 
-import {createStore} from 'redux'  // here createStore already depict or remove but we will use it
+import {createStore, applyMiddleware} from 'redux'  // import aplyMiddleware
+import logger from 'redux-logger' //import logger 
 
 //to make Store
-const store = createStore(reducer); // reducer are some functions
+const store = createStore(reducer, applyMiddleware(logger.default)); // using logger because it is a middleware
+
+
 
 //initialize state here
 function reducer(state={amount: 1}, action){ // state means previous state and action means the action we doing like add, remove etc.
@@ -57,7 +34,18 @@ function reducer(state={amount: 1}, action){ // state means previous state and a
 console.log(store.getState())
 
 //action with dispatch means like click function
-store.dispatch({type:'increment'})
+store.dispatch({type:'increment'}) // increment is predefined to increase value
 
 // new state(changes)
 console.log(store.getState())
+
+
+/* output:
+{ amount: 1 }
+ action increment @ 14:48:10.794
+   prev state { amount: 1 }
+   action     { type: 'increment' }
+   next state { amount: 2 }
+{ amount: 2 }
+
+*/
