ce7882d4d8053a54214c0e7d8b26f55bff15276b
Niti9 <nnegi8298@gmail.com>
2023-06-14 08:01:17 +0530

    thunk middleware used  and take data from port:3000 Yahan humein cmd mein json-server db.json bhi run karna hogat kyunki data wahin se aayega aur yahan se wahan incrementByAmount wala action perform jayega

--- index.js
+++ index.js
@@ -17,7 +17,8 @@
 
 import { createStore, applyMiddleware } from 'redux'  // import aplyMiddleware
 import logger from 'redux-logger' //import logger 
-
+import axios from 'axios';
+import thunk from 'redux-thunk';
 
 // action name constants (used to resolve the conflict kyunki increment , decrement wagarh bhot baar repeat ho rha hai 
 // if condition  mein aur action creator aur dispatch mein bhi)
@@ -28,7 +29,7 @@ const init = 'init';
 
 
 //to make Store
-const store = createStore(reducer, applyMiddleware(logger.default)); // using logger because it is a middleware
+const store = createStore(reducer, applyMiddleware(logger.default, thunk.default)); // using logger because it is a middleware
 
 
 
@@ -55,9 +56,19 @@ function reducer(state = { amount: 1 }, action) { // state means previous state 
 }
 
 
+//Async API call
+async function getUser() {
+    const { data } = await axios.get('http://localhost:3000/accounts/1') //ye db.json ka accounts ka data le rhe hai
+    console.log(data)
+}
+
+getUser();
+
+
 //Action Creator
-function initUser(value) { // new action for thunk 
-    return { type: init, payload: value}
+async function initUser(dispatch,getState) { // new action for thunk (here dispatch, and getState(to get global state) used )
+    const { data } = await axios.get('http://localhost:3000/accounts/1') //ye db.json ka accounts ka data le rhe hai
+    dispatch({ type: init, payload: data.amount })
 }
 function increment() {
     return { type: inc }
@@ -77,12 +88,27 @@ function incrementByAmount(value) {
 console.log(store.getState())
 
 //calling action creator
-store.dispatch(increment()) // increment to increase value
-store.dispatch(decrement()) // decrement  to decrease the value
-store.dispatch(incrementByAmount(4)) // here payload must be given it will like a gap between the values
-store.dispatch(initUser(400)) // here payload must be given it will make gap b/w values
+// store.dispatch(increment()) // increment to increase value
+// store.dispatch(decrement()) // decrement  to decrease the value
+// store.dispatch(incrementByAmount(4)) // here payload must be given it will like a gap between the values
+store.dispatch(initUser) // here payload must be given it will make gap b/w values
 
 
 // new state(c hanges)
 console.log(store.getState())
 
+/**
+ * output:
+ * 
+{ amount: 1 }
+ action undefined @ 07:50:54.898
+   prev state { amount: 1 }
+   action     [AsyncFunction: initUser]
+   next state { amount: 1 }
+{ amount: 1 }
+{ id: 1, amount: 200 }
+ action init @ 07:50:54.962
+   prev state { amount: 1 }
+   action     { type: 'init', payload: 200 }
+   next state { amount: 200 }
+ */
